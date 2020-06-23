031a51cd0972f613a240bb57d0cac3e7d0c392ac
Ridi113 <ridixrx@gmail.com>
2020-06-23 11:25:58 +0600

    Chat application is created using Flask-SocketIO

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from wtform_fields import *
 from models import *
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import time
+from time import localtime, strftime
 
 from passlib.hash import pbkdf2_sha256
 
@@ -13,6 +17,13 @@ app.secret_key = 'replace later'
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://jayfgzkhxgprlt:8708ad5ec7c873aa3f587b152b5a9d82dbc1f4c4c041fed1939d431ca3810b30@ec2-34-193-117-204.compute-1.amazonaws.com:5432/d25lfpg5t060v5'
 
 db = SQLAlchemy(app)
+
+#Initialize Flask-SocketIO
+socketio = SocketIO(app, manage_session=False)
+
+# Predefined rooms for chat
+ROOMS = ["lounge", "news", "games", "coding"]
+
 
 #Configure flask login
 login = LoginManager(app)
@@ -62,8 +73,8 @@ def chat():
     if not current_user.is_authenticated:
         flash('Please login.', 'danger')
         return redirect(url_for('login'))
-    # return "Not logged in!"
-    return "Chat with me"
+
+    return render_template('chat.html', username=current_user.username, rooms=ROOMS)
 
 @app.route("/logout", methods=['GET'])
 def logout():
@@ -73,7 +84,48 @@ def logout():
     flash('You have logged out successfully', 'success')
     return redirect(url_for('login'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@socketio.on('incoming-msg')
+def on_message(data):
+
+    print(f"\n\n{data}\n\n")
+    # send(data)
+    # emit('some-event', 'this is a custom event message')
+    """Broadcast messages"""
+
+    msg = data["msg"]
+    username = data["username"]
+    room = data["room"]
+    # Set timestamp
+    time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+    send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
+
+
+@socketio.on('join')
+def on_join(data):
+    """User joins a room"""
+
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+
+    # Broadcast that new user has joined
+    send({"msg": username + " has joined the " + room + " room."}, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    """User leaves a room"""
+
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send({"msg": username + " has left the room"}, room=room)
+
 
 if __name__ == "__main__":
-
-    app.run(debug = True)
+    socketio.run(app, debug = True)
+    # app.run(debug = True)
